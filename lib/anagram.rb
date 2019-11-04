03bc26615f612794594270e1778596ac3344ccdf
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-04 06:30:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
   
   def match(array_of_words)  
     array_of_words.select do |word|
-      (@word.split(" ").sort) == (word.split(" ").sort)
+      (@word.split("").sort) == (word.split("").sort)
     end 
   end 
 end 
