a45c90008b82dfe0dd6ffe71b32cb6cb0eff3cf1
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-04 06:28:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,8 @@ class Anagram
   def initialize(word)
     @word = word 
   end 
+  
+  def match(array_of_words)  
     array_of_words.select do |word|
       (@word.split(" ").sort) == (word.split(" ").sort))
     end 
